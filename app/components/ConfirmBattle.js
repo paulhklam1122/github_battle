63961aef22879dd35194d5e7dabd10a2c3378d3e
Paul Lam <paulhklam1122@gmail.com>
2016-10-28 14:47:09 -0700

    components finished before github data fetching

---
+++ app/components/ConfirmBattle.js
@@ -0,0 +1,7 @@
+var React = require('react');
+
+function ConfirmBattle(props) {
+  return props.isLoading === true ? <p> LOADING! </p> : <p> CONFIRM BATTLE! </p>
+}
+
+module.exports = ConfirmBattle;
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,63 +2,23 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 var Prompt = require('../components/Prompt');
 
-// var PromptContainer = React.createClass({
-//   contextTypes: {
-//     router: React.PropTypes.object.isRequired
-//   },
-//   getInitialState: function() {
-//     return {
-//       username: ''
-//     }
-//   },
-//   handleUpdateUser: function(e) {
-//     this.setState({
-//       username: e.target.value
-//     })
-//   },
-//   handleSubmitUser: function(event) {
-//     event.preventDefault();
-//     var username = this.state.username;
-//     this.setState({
-//       username: ''
-//     });
-//
-//     if (this.props.routeParams.playerOne) {
-//       this.context.router.push({
-//         pathname: '/battle',
-//         query: {
-//           playerOne: this.props.routeParams.playerOne,
-//           playerTwo: this.state.username
-//         }
-//       });
-//     } else {
-//       this.context.router.push('/playerTwo/' + this.state.username)
-//     }
-//   },
-//   render: function() {
-//     return (
-//       <Prompt
-//         onSubmitUser={this.handleSubmitUser}
-//         onUpdateUser={this.handleUpdateUser}
-//         header={this.props.route.header}
-//         username={this.state.username}/>
-//     );
-//   }
-// });
-
-class PromptContainer extends React.Component {
-  static defaultProps() {
-    username: ''
-  }
-  handleUpdateUser(e) {
-    e.preventDefault();
+var PromptContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function() {
+    return {
+      username: ''
+    }
+  },
+  handleUpdateUser: function(e) {
     this.setState({
       username: e.target.value
-    });
-  }
-  handleSubmitUser(event) {
-    even.preventDefault();
-    var username = this.state.username
+    })
+  },
+  handleSubmitUser: function(event) {
+    event.preventDefault();
+    var username = this.state.username;
     this.setState({
       username: ''
     });
@@ -74,8 +34,8 @@ class PromptContainer extends React.Component {
     } else {
       this.context.router.push('/playerTwo/' + this.state.username)
     }
-  }
-  render() {
+  },
+  render: function() {
     return (
       <Prompt
         onSubmitUser={this.handleSubmitUser}
@@ -84,6 +44,46 @@ class PromptContainer extends React.Component {
         username={this.state.username}/>
     );
   }
-}
+});
+
+// class PromptContainer extends React.Component {
+//   static defaultProps() {
+//     username: ''
+//   }
+//   handleUpdateUser(e) {
+//     e.preventDefault();
+//     this.setState({
+//       username: e.target.value
+//     });
+//   }
+//   handleSubmitUser(event) {
+//     even.preventDefault();
+//     var username = this.state.username
+//     this.setState({
+//       username: ''
+//     });
+//
+//     if (this.props.routeParams.playerOne) {
+//       this.context.router.push({
+//         pathname: '/battle',
+//         query: {
+//           playerOne: this.props.routeParams.playerOne,
+//           playerTwo: this.state.username
+//         }
+//       });
+//     } else {
+//       this.context.router.push('/playerTwo/' + this.state.username)
+//     }
+//   }
+//   render() {
+//     return (
+//       <Prompt
+//         onSubmitUser={this.handleSubmitUser}
+//         onUpdateUser={this.handleUpdateUser}
+//         header={this.props.route.header}
+//         username={this.state.username}/>
+//     );
+//   }
+// }
 
 module.exports = PromptContainer;

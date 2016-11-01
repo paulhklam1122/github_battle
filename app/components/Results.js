d05793f5f4775557145ad7e1a6a24c87ad86c617
Paul Lam <paulhklam1122@gmail.com>
2016-11-01 12:00:44 -0700

    Loading component completed

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,6 +5,7 @@ var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function StartOver() {
   return (
@@ -19,7 +20,7 @@ function StartOver() {
 function Results(props) {
   if (props.isLoading === true) {
     return (
-      <p>Loading</p>
+      <Loading speed={100} text="One Moment Please" />
     );
   }
   if (props.scores[0] === props.scores[1]) {

bf68934d0ac35331f1ed39fcb6ef43fc7fb28b32
Paul Lam <paulhklam1122@gmail.com>
2016-11-01 16:19:27 -0700

    route transitions added

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,22 +1,32 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 // var Main = React.createClass({
 //   render: function() {
 //     return (
-//       <div>
-//         Hello from Main
-//         {this.props.children}
+//       <div className='main-container'>
+//         <ReactCSSTransitionGroup
+//           transitionName="appear"
+//           transitionEnterTimeout={500}
+//           transitionLeaveTimeout={500}>
+//             {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+//         </ReactCSSTransitionGroup>
 //       </div>
 //     );
 //   }
 // });
 
-
 class Main extends React.Component {
   render() {
     return (
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+            {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
